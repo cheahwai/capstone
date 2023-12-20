dd983e1a1057ab7106d0aa7c71b52cf07cf3775e
cheahwai <karwaicheah56@gmail.com>
2023-12-20 15:13:56 +0800

    update pin

--- esp32.ino
+++ esp32.ino
@@ -1,7 +1,7 @@
 //the duration for ultrasonic sensor is commend as it need recieve signal to preceed, if not will MAX DELAY
 
 #include <EEPROM.h>
-#define EEPROM_SIZE     4
+#define EEPROM_SIZE       64
 
 //#define BLUETOOTH_EN    1
 #define WIFI_EN         1
@@ -33,12 +33,14 @@ char BTstr[100] = {};
 //const char *password = NULL;
 const char *ssid = "Galaxy A13 A863";
 const char *password = "vere5058";
-const char *sheetId = "YourGoogleSheetID";
-const char *apiKey = "YourGoogleAPIKey";
+const char *sheetId = "AKfycbx7eFSIjuFf_J8FW6N-Yk0xnKe2ha0wpISU2umAAV03UJesyM0ZBOSRe36L_-d2NCZd";
 
 String sheetName = "Sheet1";
 String cell = "A1";
 int lastCol = 1;
+
+String getColumnLetter(int col);
+void updateGoogleSheet(uint32_t value);
 
 #endif
 
@@ -55,33 +57,31 @@ int lastCol = 1;
 #define ECHO_B_PIN      14
 #define SOUND_SPEED     0.034
 
-#define MOTOR_PWM_PIN   23
-#define MOTOR_DIR_PIN   22
+#define MOTOR_PWM_PIN   21
+#define MOTOR_DIR1_PIN  19
+#define MOTOR_DIR2_PIN  18
 #define MOTOR_PWM_Ch    0
-#define FRONT_PB_PIN    17
-#define BACK_PB_PIN     16
-#define stop_motor      {ledcWrite(MOTOR_PWM_Ch, 0); digitalWrite(MOTOR_DIR_PIN, 0);}
-#define move_motor      {ledcWrite(MOTOR_PWM_Ch, 6000); digitalWrite(MOTOR_DIR_PIN, 1);}
-#define return_motor    {ledcWrite(MOTOR_PWM_Ch, -6000); digitalWrite(MOTOR_DIR_PIN, 0);}
-#define front_PB        (digitalRead(FRONT_PB_PIN) == 0)
-#define back_PB         (digitalRead(BACK_PB_PIN) == 0)
+#define FRONT_LIMIT_PIN 22
+#define BACK_LIMIT_PIN  23
+#define stop_motor      {ledcWrite(MOTOR_PWM_Ch, 0); digitalWrite(MOTOR_DIR1_PIN, 0); digitalWrite(MOTOR_DIR2_PIN, 0);}
+#define forward_motor   {ledcWrite(MOTOR_PWM_Ch, 100); digitalWrite(MOTOR_DIR1_PIN, 1); digitalWrite(MOTOR_DIR2_PIN, 0);}
+#define return_motor    {ledcWrite(MOTOR_PWM_Ch, 100); digitalWrite(MOTOR_DIR1_PIN, 0); digitalWrite(MOTOR_DIR2_PIN, 1);}
+#define front_limit     digitalRead(FRONT_LIMIT_PIN)
+#define back_limit      digitalRead(BACK_LIMIT_PIN)
 
-#define VIBRATOR_PIN    21
+#define VIBRATOR_PIN    15
 
-#define LED_R_PIN       19
-#define LED_G_PIN       18
-#define LED_B_PIN       5
+#define LED_R_PIN       5
+#define LED_G_PIN       17
+#define LED_B_PIN       16
 #define LED_R_Ch        1
 #define LED_G_Ch        2
 #define LED_B_Ch        3
 
-#define FLASH_PIN       15
-#define FLASH_WRITE_PB  digitalRead(FLASH_PIN)
-
 //////////////user threshold/////////////////////
 
 float seat_threshold = 6000.0;           //value when sit
-int BACK_THRESHOLD = 3.5;               //minimum distance in cm
+int min_dist = 4.5;               //minimum distance in cm
 float seat_width = 30.0;              //in cm
 float dist_compare_threshold = 50.0;  //in percentage
 float seat_compare_threshold = 50.0;  //in percentage
@@ -115,7 +115,7 @@ uint32_t sitting_count = 0;
 uint32_t flash_count = 0;
 uint32_t sit_time = 0;
 uint32_t leave_time = 0;
-
+int motor_move = 0;
 ////////////////////////////////////////////
 
 void pressure (void);
@@ -125,18 +125,11 @@ void check (void);
 
 ////////////////////////////////////////////
 
-String getColumnLetter(int col);
-void updateGoogleSheet(uint32_t value);
-
-////////////////////////////////////////////
-
 void setup() {
   // initialize EEPROM with predefined size
   EEPROM.begin(EEPROM_SIZE);
-  pinMode(FLASH_WRITE_PB, INPUT_PULLUP);
   seat_width = EEPROM.read(0);
   
-  // put your setup code here, to run once:
 //  analogReadResolution(12);       //4096, 3.2V
 //  analogSetClockDiv(1);
 //  setCpuFrequencyMhz(80);
@@ -149,17 +142,18 @@ void setup() {
   pinMode(ECHO_B_PIN, INPUT);
 
   ledcAttachPin(MOTOR_PWM_PIN, MOTOR_PWM_Ch);
-  ledcSetup(0, 1000, 15);         //1000Hz, up to 20000 (32767, 0x7FFF)
-  pinMode(MOTOR_DIR_PIN, OUTPUT);
-  pinMode(FRONT_PB_PIN, INPUT_PULLUP);
-  pinMode(BACK_PB_PIN, INPUT_PULLUP);
+  ledcSetup(0, 100, 8);         //1000Hz, CCR up to 256
+  pinMode(MOTOR_DIR1_PIN, OUTPUT);
+  pinMode(MOTOR_DIR2_PIN, OUTPUT);
+  pinMode(FRONT_LIMIT_PIN, INPUT_PULLUP);
+  pinMode(BACK_LIMIT_PIN, INPUT_PULLUP);
 
   pinMode(VIBRATOR_PIN, OUTPUT);
   
   ledcAttachPin(LED_R_PIN, LED_R_Ch);
   ledcAttachPin(LED_G_PIN, LED_G_Ch);
   ledcAttachPin(LED_B_PIN, LED_B_Ch);
-  ledcSetup(1, 1000, 8);          //1000Hz, up to 256
+  ledcSetup(1, 1000, 8);          //1000Hz, CCR up to 256
   ledcSetup(2, 1000, 8);
   ledcSetup(3, 1000, 8);
   pinMode(LED_PIN, OUTPUT);
@@ -171,6 +165,7 @@ void setup() {
 #endif
 
 #ifdef WIFI_EN
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -187,40 +182,43 @@ void setup() {
     delay(10);
   }
   
-  // Get the current date and time
-  time_t now = time(nullptr);
-  struct tm *timeInfo = localtime(&now);
-
-  String url = "https://sheets.googleapis.com/v4/spreadsheets/" + String(sheetId) + "/values/" + sheetName + "?key=" + String(apiKey);
-
-  // Prepare HTTP GET request to read the last value in the Google Sheet
-  HTTPClient http;
-  http.begin(url);
-  int httpResponseCode = http.GET();
-
-  // Compare with the last sit_time
-  if (httpResponseCode > 0) {
-    // Parse the JSON response
-    DynamicJsonDocument jsonDoc(1024);
-    deserializeJson(jsonDoc, http.getString());
-
-    // Extract the last value from the Google Sheet
-    String lastValue = jsonDoc["values"][0][0];
-
-    // Compare with the last stored EEPROM value
-    if (lastValue.toInt() != sit_time) {
-      // Values are different, update the Google Sheet
-      updateGoogleSheet(sit_time);
-    } else {
-      Serial.println("sit_time in EEPROM is the same as the last value in Google Sheet. No update needed.");
-    }
-  } else {
-    Serial.print("HTTP Request failed. Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  http.end();
+//  // Get the current date and time
+//  time_t now = time(nullptr);
+//  struct tm *timeInfo = localtime(&now);
+//
+//  String url = "https://sheets.googleapis.com/v4/spreadsheets/" + String(sheetId) + "/values/" + sheetName;
+//
+//  // Prepare HTTP GET request to read the last value in the Google Sheet
+//  HTTPClient http;
+//  http.begin(url);
+//  int httpResponseCode = http.GET();
+//
+//  // Compare with the last sit_time
+//  if (httpResponseCode > 0) {
+//    // Parse the JSON response
+//    DynamicJsonDocument jsonDoc(1024);
+//    deserializeJson(jsonDoc, http.getString());
+//
+//    // Extract the last value from the Google Sheet
+//    String lastValue = jsonDoc["values"][0][0];
+//
+//    // Compare with the last stored EEPROM value
+//    if (lastValue.toInt() != sit_time) {
+//      // Values are different, update the Google Sheet
+//      updateGoogleSheet(sit_time);
+//    } else {
+//      Serial.println("sit_time in EEPROM is the same as the last value in Google Sheet. No update needed.");
+//    }
+//  } else {
+//    Serial.print("HTTP Request failed. Error code: ");
+//    Serial.println(httpResponseCode);
+//  }
+//  http.end();
   
 #endif
+
+  Serial.println("\nsetup done");
+  delay(10000);
 }
 
 void loop() {
@@ -238,15 +236,17 @@ void loop() {
 
   pressure();
   distance();
-//  back_support();
+  back_support();
   
   if(millis() - serial_count >= 300){
     Serial.print("\n\nCPU time: ");
     Serial.print(millis());
     Serial.print(" , PB: (front) ");
-    Serial.print(front_PB);
+    Serial.print(front_limit);
     Serial.print(" (back) ");
-    Serial.print(back_PB);
+    Serial.print(back_limit);
+    Serial.print(" (motor) ");
+    Serial.print(motor_move);
     Serial.print(" , flag: ");
     Serial.print(start_flag);
     Serial.print(", ");
@@ -275,15 +275,15 @@ void loop() {
     serial_count = millis();
   }
 
-  if(FLASH_WRITE_PB == 0 && flash_PB_flag == 0){
-    delay(20);
-    flash_PB_flag = 1;
-    seat_width = L_dist;
-    EEPROM.write(0, seat_width);
-    EEPROM.commit();
-  }else if(FLASH_WRITE_PB && flash_PB_flag){
-    flash_PB_flag = 0;
-  }
+//  if(FLASH_WRITE_PB == 0 && flash_PB_flag == 0){
+//    delay(20);
+//    flash_PB_flag = 1;
+//    seat_width = L_dist;
+//    EEPROM.write(0, seat_width);
+//    EEPROM.commit();
+//  }else if(FLASH_WRITE_PB && flash_PB_flag){
+//    flash_PB_flag = 0;
+//  }
 
   if(millis() - flash_count >= 60 * 1000){
     flash_count = millis();
@@ -320,26 +320,26 @@ void pressure(void){
 }
 
 void back_support (void){
-  static uint8_t move_support = 1;
-  if(start_flag == 0){
-    move_support = 1;
-    return;
-  }
   digitalWrite(TRIG_B_PIN, 0);
   delay(2);
   digitalWrite(TRIG_B_PIN, 1);
   delay(10);
   digitalWrite(TRIG_B_PIN, 0);
   long durationB = pulseIn(ECHO_B_PIN, 1);
-//  long durationB;
 
   Back_dist = durationB * SOUND_SPEED / 2;
-  if(move_support){
-    move_motor
-    if(Back_dist <= BACK_THRESHOLD || back_PB){
-      stop_motor
-      move_support = 0;
-    }
+  bool limitSwitch1 = (back_limit == 0);   // 
+  bool limitSwitch2 = (front_limit == 0);  // Active LOW
+
+  if (Back_dist > min_dist && Back_dist < seat_width && !limitSwitch1 && start_flag) {
+    forward_motor
+    motor_move = 1;
+  } else if (Back_dist >= seat_width && !limitSwitch2) {
+    return_motor
+    motor_move = 2;
+  } else {
+    stop_motor
+    motor_move = 3;
   }
 }
 
@@ -365,12 +365,11 @@ void distance (void){
   digitalWrite(TRIG_L_PIN, 0);
   long duration2 = pulseIn(ECHO_L_PIN, 1);
   
-//  long duration1, duration2;
   H_dist = duration1 * SOUND_SPEED / 2;
   L_dist = duration2 * SOUND_SPEED / 2;
 
-  if(H_dist <= 3.0 || L_dist <= 3.0)     //if not detected or user's back touched to back
-    Diff_dist = 0.0;
+  if(H_dist <= min_dist || L_dist <= min_dist)     //if not detected or user's back touched to back
+    Diff_dist = 3.0;
   else if(H_dist > L_dist)
     Diff_dist = (H_dist - L_dist) / H_dist * 100.0;
   else
@@ -381,9 +380,7 @@ void check (void){
   if(start_flag == 0){
     sitting_count = millis();
     if(update_en && millis() - leave_count >= 3 * 60 * 1000){
-
       update_en = 0;
-
 #ifdef BLUETOOTH_EN
     if (SerialBT.available()) {
       Serial.write(SerialBT.read());
@@ -397,10 +394,6 @@ void check (void){
 #endif
 
     }else if(millis() - leave_count >= STAND_TIME){
-      if(front_PB)
-        stop_motor
-      else
-        return_motor
       alert = 5;
     }else
       alert = 0;
@@ -495,13 +488,14 @@ void check (void){
   }   
 }
 
+#ifdef WIFI_EN
 void updateGoogleSheet(uint32_t value) {
   // Retrieve the last-used cell information
   int lastRow = 1;  // You may adjust this based on your specific requirements
   int lastCol = 1;  // You may adjust this based on your specific requirements
 
   // Build the URL for the Google Sheets API to add the new value
-  String addUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + String(sheetId) + "/values/" + sheetName + "!" + getColumnLetter(lastCol) + String(lastRow) + "?key=" + String(apiKey);
+  String addUrl = "https://sheets.googleapis.com/v4/spreadsheets/" + String(sheetId) + "/values/" + sheetName + "!" + getColumnLetter(lastCol) + String(lastRow);
 
   // Prepare HTTP PUT request
   HTTPClient http;
@@ -545,3 +539,5 @@ String getColumnLetter(int col) {
   }
   return columnLetter;
 }
+
+#endif

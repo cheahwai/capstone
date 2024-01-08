c93f73cf652504a80c836a04818c8ad7a34ec149
cheahwai <karwaicheah56@gmail.com>
2024-01-08 23:16:37 +0800

    Appdone

--- esp32.ino
+++ esp32.ino
@@ -26,13 +26,16 @@ uint8_t update_en = 0;
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+#include <WebServer.h>
 
-const char *ssid = "";
-const char *password = "";
+const char *ssid = "Galaxy A13 A863";
+const char *password = "vere5058";
 const char* googleAppsScriptURL = "https://script.google.com/macros/s/AKfycbzsbIjGoYmz-6omRmBLgRw18gZpx0hVqXdTKmaJhBPX3GhRZJmLlRUgleFoWIShLuTH/exec";
 
 HTTPClient http;
-uint8_t update_en = 0;
+WebServer server(80);
+int address = 0;
+float value = 0.0;
 
 String getColumnLetter(int col);
 void updateGoogleSheet(uint32_t value);
@@ -43,16 +46,18 @@ void updateGoogleSheet(uint32_t value);
 
 #define PRESSURE1_PIN   34
 #define PRESSURE2_PIN   35
+#define PRESSURE3_PIN   32
+#define PRESSURE4_PIN   33
 
-#define TRIG_H_PIN      32
-#define ECHO_H_PIN      33
-#define TRIG_L_PIN      25
-#define ECHO_L_PIN      26
-#define TRIG_B_PIN      27
-#define ECHO_B_PIN      14
+#define TRIG_H_PIN      25
+#define ECHO_H_PIN      26
+#define TRIG_L_PIN      27
+#define ECHO_L_PIN      14
 #define SOUND_SPEED     0.034
 
-#define VIBRATOR_PIN    15
+#define VIBRATOR_PIN    21
+
+#define AUDIO_PIN       19
 
 #define LED_R_PIN       5
 #define LED_G_PIN       17
@@ -63,11 +68,12 @@ void updateGoogleSheet(uint32_t value);
 
 //////////////user threshold/////////////////////
 
-float seat_threshold = 6000.0;        //threshold value when sit
-int min_dist = 4.5;                   //minimum distance in cm
-float seat_width = 30.0;              //in cm
-float dist_compare_threshold = 50.0;  //in percentage
-float seat_compare_threshold = 50.0;  //in percentage
+float seat_threshold = 6600.0;        //threshold value when sit
+uint16_t back_threshold = 3800.0;     //threshold for back touching
+int min_dist = 3.5;                   //minimum distance in cm
+float seat_width = 42.0;              //in cm
+float dist_compare_threshold = 10.0;  //in cm
+float seat_compare_threshold = 23.0;  //in percentage
 uint64_t SIT_TIME = 45 * 60 * 1000;   //45 minutes
 uint64_t STAND_TIME = 3 * 60 * 1000;  //3 minutes
 
@@ -82,17 +88,21 @@ uint8_t led1 = 0;
 uint8_t ledR_PWM = 0, ledG_PWM = 0, ledB_PWM = 0;
 
 uint16_t pressure1_data = 0, pressure2_data = 0;
+uint16_t pressure3_data = 0, pressure4_data = 0;
 float left_pressure = 0, right_pressure = 0;
 float Diff_pressure = 0.0;
+uint8_t back_touched = 0;
 
 uint8_t flash_PB_flag = 0;
 
 uint32_t led_count = 0;
-uint32_t BT_count = 0;
 uint32_t serial_count = 0;
 uint32_t leave_count = 0;
 uint32_t sitting_count = 0;
 uint32_t flash_count= 0;
+uint32_t back_count = 0;
+uint32_t side_count = 0;
+uint32_t front_count = 0;
 uint32_t sit_time = 0;
 uint32_t leave_time = 0;
    
@@ -100,6 +110,7 @@ uint32_t leave_time = 0;
 
 void pressure (void);
 void distance (void);
+void back_support(void);
 void check (void);
 
 ////////////////////////////////////////////
@@ -107,10 +118,15 @@ void check (void);
 void setup() {
 
   EEPROM.begin(EEPROM_SIZE);
-  seat_width = EEPROM.read(0);
-    //    EEPROM.write(0, seat_width);
-  //    EEPROM.commit();
-
+  // seat_width = EEPROM.read(0);
+//  EEPROM.put(0, SIT_TIME);
+//  EEPROM.put(sizeof(uint64_t), seat_compare_threshold);
+//  EEPROM.put(sizeof(uint64_t) + sizeof(float), seat_width);
+//  EEPROM.commit();
+  EEPROM.get(0, SIT_TIME);
+  EEPROM.get(sizeof(uint64_t), seat_compare_threshold);
+  EEPROM.get(sizeof(uint64_t) + sizeof(float), seat_width);
+    
   //  analogReadResolution(12);       //4096, 3.2V
   //  analogSetClockDiv(1);
   //  setCpuFrequencyMhz(80);
@@ -144,7 +160,29 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
+  Serial.println(WiFi.localIP());
 
+  server.on("/someEndpoint", HTTP_GET, []() {
+    address = server.arg("address").toInt();
+    value = server.arg("value").toFloat();
+
+    if(address == 1){
+        SIT_TIME = value * 60 * 1000;
+        EEPROM.put(0, SIT_TIME);
+        EEPROM.commit();
+        server.send(200, "text/plain", "Signal received by ESP32");
+    }else if(address == 2){
+        seat_compare_threshold = Diff_pressure + 3.0;
+        EEPROM.put(sizeof(uint64_t), seat_compare_threshold);
+        EEPROM.commit();
+        server.send(200, "text/plain", "Signal received by ESP32");
+    }else if(address == 3){
+        server.send(200, "text/plain", String(alert));
+    }
+  });
+
+  server.begin();
+  
 #endif
 
   Serial.println("\nsetup done, start running in ");
@@ -153,11 +191,15 @@ void setup() {
     Serial.print("s ");
     delay(1000);
   }
+  back_count = millis();
+  front_count = millis();
+  side_count = millis();
 }
 
 void loop() {
-
+  server.handleClient();
   pressure();
+  back_support();
   distance();
 
   sit_time = (millis() - sitting_count) / 1000;
@@ -171,51 +213,18 @@ void loop() {
   }
 
   if(millis() - serial_count >= 500){
-//
-//    Serial.print("\n\nCPU time: ");
-//    Serial.print(millis());
-//    Serial.print(" , PB: (front) ");
-//    Serial.print(front_limit);
-//    Serial.print(" (back) ");
-//    Serial.print(back_limit);
-//    Serial.print(" (motor) ");
-//    Serial.print(motor_move);
-//    Serial.print(" , flag: ");
-//    Serial.print(start_flag);
-//    Serial.print(", ");
-//    Serial.println(alert);
-//
-//    Serial.print("Back_dist: ");
-//    Serial.print(Back_dist);
-//    Serial.print("  Upper_dist: ");
-//    Serial.print(H_dist);
-//    Serial.print("  Lower_dist: ");
-//    Serial.print(L_dist);
-//    Serial.print("  Diff_dist (%): ");
-//    Serial.println(Diff_dist);
-//
-//    Serial.print("Pressure: ");
-//    Serial.print(left_pressure);
-//    Serial.print(", ");
-//    Serial.print(right_pressure);
-//    Serial.print(", Pressure_dist (%): ");
-//    Serial.println(Diff_pressure);
-//
-//    Serial.print("Time (s): (sit) ");
-//    Serial.print(sit_time);
-//    Serial.print(", (stand) ");
-//    Serial.println(leave_time);
-
     Serial.println();
+    
+    Serial.print(SIT_TIME / 60 / 1000);
+    Serial.print(" min, ");
+    Serial.print(seat_compare_threshold);
+    Serial.println("%");
+    
     Serial.print(start_flag);
     Serial.print(", ");
     Serial.println(alert);
 
-    Serial.print(H_dist);
-    Serial.print("   ");
-    Serial.print(L_dist);
-    Serial.print("   ");
-    Serial.println(Diff_dist);
+    Serial.println(H_dist);
 
     Serial.print(left_pressure);
     Serial.print("   ");
@@ -223,18 +232,34 @@ void loop() {
     Serial.print("   ");
     Serial.println(Diff_pressure);
 
+    Serial.print(pressure3_data);
+    Serial.print("   ");
+    Serial.print(pressure4_data);
+    Serial.print("   ");
+    Serial.println(back_touched);
+
     Serial.print(sit_time);
     Serial.print("   ");
-    Serial.println(leave_time);
+    Serial.print(leave_time);
+    Serial.print("   ");
+    Serial.print((millis() - side_count)/1000);
+    Serial.print("   ");
+    Serial.print((millis() - front_count)/1000);
+    Serial.print("   ");
+    Serial.println((millis() - back_count)/1000);
 
 #ifdef BLUETOOTH_EN
+    sprintf(BTstr, "flash: %u %f\n", SIT_TIME / 60 / 1000, seat_compare_threshold);
+    SerialBT.write((uint8_t *)&BTstr, strlen(BTstr));
     sprintf(BTstr, "flag: %u %d\n", start_flag, alert);
     SerialBT.write((uint8_t *)&BTstr, strlen(BTstr));
-    sprintf(BTstr, "dist: %.2f %.2f %.2f%%\n", H_dist, L_dist, Diff_dist);
+    sprintf(BTstr, "dist: %.2f \n", H_dist);
     SerialBT.write((uint8_t *)&BTstr, strlen(BTstr));
     sprintf(BTstr, "pres: %.2f %.2f %.2f%%\n", left_pressure, right_pressure, Diff_pressure);
     SerialBT.write((uint8_t *)&BTstr, strlen(BTstr));
-    sprintf(BTstr, "time: %u %u\n\n", sit_time, leave_time);
+    sprintf(BTstr, "back: %d %d %d\n", pressure3_data, pressure4_data, back_touched);
+    SerialBT.write((uint8_t *)&BTstr, strlen(BTstr));
+    sprintf(BTstr, "time: %u %u %u %u %u\n\n", sit_time, leave_time, (millis() - side_count)/1000, (millis() - front_count)/1000, (millis() - back_count)/1000);
     SerialBT.write((uint8_t *)&BTstr, strlen(BTstr));
 #endif
     serial_count = millis();
@@ -242,8 +267,6 @@ void loop() {
 
   if(millis() - flash_count >= 60 * 1000 && start_flag){
     flash_count = millis();
-//    EEPROM.put(2, sit_time);
-//    EEPROM.commit();
 
 #ifdef WIFI_EN
     if (WiFi.status() != WL_CONNECTED) {
@@ -265,6 +288,15 @@ void pressure(void){
 
   start_flag = ((left_pressure + right_pressure) < seat_threshold);
   Diff_pressure = abs(left_pressure - right_pressure) / max(left_pressure, right_pressure) * 100.0;
+  if(Diff_pressure < seat_compare_threshold && start_flag)  side_count = millis();
+}
+
+void back_support (void){
+  pressure3_data = analogRead(PRESSURE3_PIN);
+  pressure4_data = analogRead(PRESSURE4_PIN);
+
+  back_touched = (start_flag && (pressure3_data < back_threshold || pressure4_data < back_threshold));
+  if(back_touched && start_flag) back_count = millis();
 }
 
 float get_dist (int TRIG_PIN, int ECHO_PIN){
@@ -285,10 +317,16 @@ void distance (void){
   }
 
   H_dist = get_dist(TRIG_H_PIN, ECHO_H_PIN);
-  L_dist = get_dist(TRIG_L_PIN, ECHO_L_PIN);
+//  L_dist = get_dist(TRIG_L_PIN, ECHO_L_PIN);
+//  L_dist = min_dist;
 
-  Diff_dist = (H_dist <= min_dist || L_dist <= min_dist) ? 3.0 :
-            abs(H_dist - L_dist) / max(H_dist, L_dist) * 100.0;
+  if(H_dist > 60.00)  H_dist = min_dist;
+//  if(L_dist > 60.00)  L_dist = min_dist;
+
+//  Diff_dist = (start_flag == 0) ? 0.0 :
+//            (H_dist < min_dist || L_dist < min_dist) ? 3.0 :
+//            abs(H_dist - L_dist) / max(H_dist, L_dist) * 100.0;
+  if(H_dist <= dist_compare_threshold) front_count = millis();
 }
 
 void output_device(int vibrator_en, int RLED_en, int GLED_en, int BLED_en){
@@ -301,10 +339,15 @@ void output_device(int vibrator_en, int RLED_en, int GLED_en, int BLED_en){
   }else{
       digitalWrite(VIBRATOR_PIN, 0);
   }
+//  digitalWrite(VIBRATOR_PIN, vibrator_en);
   ledR_PWM = RLED_en? ledR_PWM + 5 : 0;
   ledG_PWM = GLED_en && RLED_en? ledG_PWM + 2 :
              GLED_en? ledG_PWM + 2 : 0;
   ledB_PWM = BLED_en? ledB_PWM + 5 : 0;
+  if(RLED_en && GLED_en && ledR_PWM >= 250){
+    ledR_PWM = 5;
+    ledG_PWM = 5;
+  }
   if(ledR_PWM >= 250) ledR_PWM = 5;
   if(ledG_PWM >= 250) ledG_PWM = 5;
   if(ledB_PWM >= 250) ledB_PWM = 5;
@@ -332,25 +375,24 @@ void check (void){
 
   }else{
     leave_count = millis();
+    if(millis() - sitting_count >= SIT_TIME)  //time
+      alert = 4;
+    else if(back_touched == 0 && (millis() - back_count >= 5 * 1000))   //back
+      alert = 2;
+    else if(H_dist > dist_compare_threshold && (millis() - front_count >= 5 * 1000)) //front
+      alert = 1;
+    else if(Diff_pressure > seat_compare_threshold && (millis() - side_count >= 5 * 1000))   //side
+      alert = 3;
+    else
+      alert = 0;
 
 #ifdef BLUETOOTH_EN
     if(millis() - sitting_count >= 3 * 60 * 1000 && update_en == 0){
       update_en = 1;
     }
 #endif
-
-    if(millis() - sitting_count >= SIT_TIME)
-      alert = 4;
-    else if(L_dist > seat_width)
-      alert = 2;
-    else if(Diff_dist > dist_compare_threshold)
-      alert = 1;
-    else if(Diff_pressure > seat_compare_threshold)
-      alert = 3;
-    else
-      alert = 0;
   }
-
+  
   switch(alert){
   case 0:   //green
     output_device(0, 0, 1, 0);
